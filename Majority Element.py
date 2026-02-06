bbc46ae846b4244d4887e1d2d4a26501a0ea1d65
Abirham_Gedefaw <gedefawabirham@gmail.com>
2026-02-06 22:19:36 +0300

    Update Majority Element.py

--- Majority Element.py
+++ Majority Element.py
@@ -13,3 +13,23 @@ class Solution(object):
             count += (1 if num == candidate else -1)
         
         return candidate
+
+
+""" or in a more descriptive way """
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        hash_map = {}
+        for i in nums:
+            if i not in hash_map:
+                hash_map[i] = 1
+            else:
+                hash_map[i] += 1
+
+        majority, val = 0, 0
+        for key, value in hash_map.items():
+            if value > val:
+                val = value
+                majority = key
+
+        return majority
