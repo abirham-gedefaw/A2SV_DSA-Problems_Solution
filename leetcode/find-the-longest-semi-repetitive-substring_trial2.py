6d35c8747d94e6d6102a36e54a27bc1bbc4d2f59
Abirham_Gedefaw <gedefawabirham@gmail.com>
2026-03-27 15:03:01 +0300

    Add solution for Find the Longest Semi-Repetitive Substring (trial 2)

---
+++ leetcode/find-the-longest-semi-repetitive-substring_trial2.py
@@ -0,0 +1,14 @@
+class Solution:
+    def longestSemiRepetitiveSubstring(self, s: str) -> int:
+
+        adjacent_pair = 0
+        max_window_size = 1
+        left = 0
+        for right in range(1, len(s)):
+            adjacent_pair += s[right] == s[right - 1]
+            while adjacent_pair > 1:
+                adjacent_pair -= s[left] == s[left + 1]
+                left += 1
+            max_window_size = max(max_window_size, right - left + 1)
+        return max_window_size
+        
