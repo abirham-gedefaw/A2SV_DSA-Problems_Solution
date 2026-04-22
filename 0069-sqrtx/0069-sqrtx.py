32463497f5970956e6d14ac8ddd81204e2b074d4
Abirham_Gedefaw <gedefawabirham@gmail.com>
2026-04-22 21:26:14 +0300

    Time: 3 ms (67.27%), Space: 19.2 MB (63.36%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -10,5 +10,14 @@ class Solution:
             else:
                 high = mid - 1
         return ans
+
+        # the other more efficent code willbe as follow:
+        if x == 0:
+            return 0
+        guess = x
+        while guess*guess > x:
+            guess = (guess + x // guess)//2
+        return guess
+            
           
         
