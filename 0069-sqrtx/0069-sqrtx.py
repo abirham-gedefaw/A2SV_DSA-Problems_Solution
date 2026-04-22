40030b469415112cdaf802e680010303cb802ef0
Abirham_Gedefaw <gedefawabirham@gmail.com>
2026-04-22 21:10:40 +0300

    Time: 5 ms (36.42%), Space: 19.5 MB (9.94%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -3,7 +3,7 @@ class Solution:
         low, high = 1, x
         ans = 0
         while low <= high:
-            mid = low +(high - low) // 2
+            mid = low + (high - low) // 2
             if mid <= x // mid:
                 ans = mid
                 low = mid + 1
